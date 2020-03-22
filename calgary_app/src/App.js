ed5c8d7042de874d7db1abbb9c7af1c393a8808d
jcbain <jamescbain@gmail.com>
2020-03-22 01:30:53 -0600

    add react map

---
+++ calgary_app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Map from './components/Map';
+
+class App extends Component{
+
+    render(){
+        return(
+            <Map />
+        )
+    }
+}
+
+export default App;
