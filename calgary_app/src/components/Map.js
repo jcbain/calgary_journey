fbf6c76f2e5c36f02fd51ada6929778d7853d950
jcbain <jamescbain@gmail.com>
2020-03-30 22:54:30 -0600

    create rest of states

--- calgary_app/src/components/Map.js
+++ calgary_app/src/components/Map.js
@@ -4,6 +4,8 @@ import { geoAlbers, geoPath  } from 'd3-geo';
 
 import './styles/map.css';
 
+import states from '../data/us-states';
+import provinces from '../data/canada';
 import territories from '../data/selected_states_provinces';
 import routes from '../data/to_calgary';
 
@@ -12,12 +14,14 @@ class Map extends Component {
         super(props)
         this.territories = territories.features;
         this.route = routes.features;
+        this.states = states.features;
+        this.provinces = provinces.features;
         this.coords = [
             [-114.0719, 51.0447],
             [-92.3341, 38.9517]
         ];
         this.state = {
-            dims: {width: 100, height: 50},
+            dims: {width: 1000, height: 500},
             territories: [],
         }
     }
@@ -27,13 +31,31 @@ class Map extends Component {
 
 
         const projection = geoAlbers()
-            .scale([120])
+            .scale([this.state.dims.width])
             .rotate([102, 0]) // longitude center
             .center([0, 48]) // latitude center
             .translate([this.state.dims.width/2, this.state.dims.height/2]);
 
         const path = geoPath()
             .projection(projection);
+
+
+        select(this.mapRef.current)
+            .selectAll(".states")
+            .data(this.states)
+            .enter()
+            .append("path")
+            .attr("class", "states")
+            .attr("d", path);
+
+        select(this.mapRef.current)
+            .selectAll(".provinces")
+            .data(this.provinces)
+            .enter()
+            .append("path")
+            .attr("class", "provinces")
+            .attr("d", path);
+
 
         select(this.mapRef.current)
             .selectAll(".base")
@@ -51,7 +73,7 @@ class Map extends Component {
             .attr("class", "routes")
             .attr("d", path)
             .style('fill', 'none')
-            .style('stroke-width', .5);
+            .style('stroke-width', 5);
 
         select(this.mapRef.current)
             .selectAll(".city")
@@ -61,7 +83,7 @@ class Map extends Component {
             .attr("class", "city")
             .attr("cx", function(d) { return projection(d)[0]; })
             .attr("cy", function(d) { return projection(d)[1]; })
-            .attr("r", .5)
+            .attr("r", 5)
 
       
     }
